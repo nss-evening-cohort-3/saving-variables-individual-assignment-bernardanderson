452b1641b8b31680c81b86d2363f3ec45507677b
Bernard Anderson <bernard.anderson@gmail.com>
2016-09-18 21:56:50 -0500

    Sort and output 'help'

--- SavingVariables/NonDatabaseCommands.cs
+++ SavingVariables/NonDatabaseCommands.cs
@@ -10,35 +10,37 @@ namespace SavingVariables
     {
         Dictionary<string, string> simpleUserCommands = new Dictionary<string, string>
         {
-            { "lastq", "Prints the last entered command or expression" },
-            { "exit", "Exits the program" },
-            { "clear|quit", "Exits the program" },
-            { "remove|show|delete", "Can be used to delete a variable (variable letter) or the entire database (all)" }
+            { "lastq", "Prints the Last Entered Command or Expression" },
+            { "exit|quit", "Exits the Program" },
+            { "clear|delete|remove", "Used to delete a variable (variable letter) or the entire database (all)" },
+            { "show all", "List all set variables in a tabular format" }
         };
 
         public UserEntryData RouteUserCommandToCorrectMethod(UserEntryData sentUserEntryData)
         {
             if (sentUserEntryData.UserCommand == "help")
             {
-                sentUserEntryData.consoleOutputString = "This is help";
+                sentUserEntryData.consoleOutputString = ReturnListOfCommandsWhenHelpIsEntered();
             }
             else if (sentUserEntryData.UserCommand == "exit" || sentUserEntryData.UserCommand == "quit")
             {
                 sentUserEntryData.WantsToExit = true;
                 sentUserEntryData.consoleOutputString = "Goodbye!";
-            } else if (sentUserEntryData.UserCommand == "help")
-            {
-                sentUserEntryData.consoleOutputString = ReturnListOfCommandsWhenHelpIsEntered();
             }
             return sentUserEntryData;
         }
 
         public string ReturnListOfCommandsWhenHelpIsEntered()
         {
-            string 
+            string helpCommandOutput = " **Current list of user commands**\n";
+            List<string> tempKeyList = simpleUserCommands.Keys.ToList();
+            tempKeyList.Sort();
 
-            return sentUserEntryData;
+            foreach (string key in tempKeyList)
+            {
+                helpCommandOutput += $"   {key}:  {simpleUserCommands[key]}\n";
+            }
+            return helpCommandOutput;
         }
-
     }
 }
